f75673335037c03539454ad6744ae235a5dae291
ren-hoek <ren-hoek@users.noreply.github.com>
2016-11-21 08:25:00 +0000

    Got line chart working

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import json
 app = Flask(__name__)
 
 #Needs setting as CSRF_ENABLED=True for flask-wtf. Keep secret_key, secret
-app.secret_key = ''
+app.secret_key = '7923847kajsdhakjdsh'
 
 @app.route('/')
 def home():
@@ -13,6 +13,10 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/ixbrl')
+def ixbrl():
+    return render_template('ixbrl.html')
 
 @app.route('/data')
 def data():
